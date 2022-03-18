dd17344f78de5eb6390a4e1956abb6df8b492064
Asus <imamsetiawan27062000@gmail.com>
2022-03-18 09:49:55 +0700

    Collections

---
+++ src/main/kotlin/6. Collections.kt
@@ -0,0 +1,10 @@
+fun printAll(strings: Collection<String>) {
+    for(s in strings) print("$s ")
+    println()
+}
+fun main(args: Array<String>) {
+    val stringList = listOf("one", "two", "one")
+    printAll(stringList)
+    val stringSet = setOf("one", "two", "three")
+    printAll(stringSet)
+}
