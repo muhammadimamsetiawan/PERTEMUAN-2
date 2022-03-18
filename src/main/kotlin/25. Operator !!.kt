7737e087abd22dd305e79ca15996c2d1afd77a43
Asus <imamsetiawan27062000@gmail.com>
2022-03-18 11:22:56 +0700

    Operator !!

---
+++ src/main/kotlin/25. Operator !!.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var maybeNumber: Int? = 15
+    maybeNumber = null
+    println(maybeNumber * 2)
+    var s1 = "Hello"
+    var s2: String? = "World"
+    println(s1.length)
+    println(s2.length)
+}
