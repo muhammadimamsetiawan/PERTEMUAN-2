e5a2ab00041e84f143370c512bdf7643b2c84e17
Asus <imamsetiawan27062000@gmail.com>
2022-03-18 11:13:44 +0700

    While dan Do-While Loop

---
+++ src/main/kotlin/20. While dan Do-While Loop.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    //While Loop
+    var x:Int = 0
+    println("Example of While Loop--")
+    while(x <= 10) {
+        print("$x ")
+        x++
+    }
+    print('\n')
+    //Do-While Loop
+    var y:Int = 0
+    do {
+        y = y + 10
+        println("I am inside Do block---" +y)
+    } while(y <= 50)
+}
